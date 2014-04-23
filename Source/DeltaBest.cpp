1540a2cb423c10459f889e3a56bc9adf585942f6
Cosimo Streppone <cosimo@streppone.it>
2014-04-23 12:21:05 +0200

    First working version!!! YEAAH!
    Probably buggy, yes, but hell yeah, it works.
    So nice ;-)

--- Source/DeltaBest.cpp
+++ Source/DeltaBest.cpp
@@ -34,17 +34,21 @@ void __cdecl DestroyPluginObject(PluginObject *obj)  { delete((DeltaBestPlugin *
 
 
 //
-// Static plugin context data
+// Current status
 //
 
-bool in_realtime = false;
-bool green_flag = false;
-unsigned int scoring_ticks = 0;
-double current_delta_best = NULL;
+bool in_realtime = false;              /* Are we in cockpit? As opposed to monitor */
+bool green_flag = false;               /* Is the race in green flag condition? */
+unsigned int last_pos = 0;             /* Meters around the track of the current lap */
+unsigned int scoring_ticks = 0;        /* Advances every time UpdateScoring() is called */
+double current_delta_best = NULL;      /* Current calculated delta best time */
+double prev_lap_dist = 0;              /* Used to accurately calculate dt and */
+double prev_current_et = 0;            /*   speed of last interval */
 
+/* Keeps information about last and best laps */
 struct LapTime {
-	double distance[10000];
-	double elapsed[10000];
+	//double distance[10000];
+	double elapsed[50000];             /* Longest possible track is 50km */
 	double final = NULL;
 	double started = NULL;
 	double ended = NULL;
@@ -146,17 +150,20 @@ void DeltaBestPlugin::UpdateScoring(const ScoringInfoV01 &info)
 		if (! vinfo.mIsPlayer)
 			continue;
 
-		fprintf(out_file, "mLapStartET=%.3f mCurrentET=%.3f Elapsed=%.3f mLapDist=%.3f/%.3f ticks=%d\n",
+		fprintf(out_file, "mLapStartET=%.3f mCurrentET=%.3f Elapsed=%.3f mLapDist=%.3f/%.3f prevLapDist=%.3f prevCurrentET=%.3f deltaBest=%+2.2f\n",
 			vinfo.mLapStartET,
 			info.mCurrentET,
 			(info.mCurrentET - vinfo.mLapStartET),
 			vinfo.mLapDist,
 			info.mLapDist,
-			scoring_ticks
+			prev_lap_dist,
+			prev_current_et,
+			current_delta_best
 		);
 
 		/* Check if we started a new lap just now */
 		bool new_lap = (vinfo.mLapStartET != last_lap.started);
+		double curr_lap_dist = vinfo.mLapDist >= 0 ? vinfo.mLapDist : 0;
 
 		if (new_lap) {
 			
@@ -187,18 +194,54 @@ void DeltaBestPlugin::UpdateScoring(const ScoringInfoV01 &info)
 			last_lap.final = NULL;
 			last_lap.ended = NULL;
 			last_lap.interval_offset = info.mCurrentET - vinfo.mLapStartET;
+			last_lap.elapsed[0] = 0;
 			scoring_ticks = 0;
+			last_pos = 0;
+			prev_lap_dist = 0;
+			/* Leave prev_current_et alone, or you have hyper-jumps */
 		}
 
 		/* If there's a lap in progress, save the delta updates */
 		if (last_lap.started > 0.0) {
-			last_lap.distance[scoring_ticks] = vinfo.mLapDist;
-			last_lap.elapsed[scoring_ticks] = info.mCurrentET - vinfo.mLapStartET;
+			unsigned int meters = round(vinfo.mLapDist >= 0 ? vinfo.mLapDist : 0);
+
+			/* It could be that we have stopped our vehicle.
+			   In that case (same array position), we want to
+			   overwrite the previous value anyway */
+			if (meters >= last_pos) {
+				double distance_traveled = (vinfo.mLapDist - prev_lap_dist);
+				if (distance_traveled < 0)
+					distance_traveled = 0;
+				double time_interval = (info.mCurrentET - prev_current_et);
+				double avg_speed = (time_interval == 0)	? 0	: distance_traveled / time_interval;
+
+                if (meters == last_pos) {
+                    last_lap.elapsed[meters] = info.mCurrentET - vinfo.mLapStartET;
+                    fprintf(out_file, "[DELTA]     elapsed[%d] = %.3f [same position]\n", last_lap.elapsed[meters]);
+                }
+                else {
+                    for (unsigned int i = last_pos; i <= meters; i++) {
+                        /* Linear interpolation of elapsed time in relation to physical position */
+                        double interval_fraction = meters == last_pos ? 1.0 : (1.0 * i - last_pos) / (1.0 * meters - last_pos);
+                        last_lap.elapsed[i] = prev_current_et + (interval_fraction * time_interval);
+                        last_lap.elapsed[i] -= vinfo.mLapStartET;
+                        fprintf(out_file, "[DELTA]     elapsed[%d] = %.3f (interval_fraction=%.3f)\n", i, last_lap.elapsed[i], interval_fraction);
+                    }
+                }
+
+				fprintf(out_file, "[DELTA] distance_traveled=%.3f time_interval=%.3f avg_speed=%.3f [%d .. %d]\n",
+					distance_traveled, time_interval, avg_speed, last_pos, meters);
+			}
+
+			last_pos = meters;
 		}
 
-		current_delta_best = CalculateDeltaBest();
+        if (curr_lap_dist > prev_lap_dist)
+            prev_lap_dist = curr_lap_dist;
 
-		scoring_ticks++;
+        prev_current_et = info.mCurrentET;
+
+		current_delta_best = CalculateDeltaBest();
 
 	}
 
@@ -245,55 +288,73 @@ void DeltaBestPlugin::RenderScreenBeforeOverlays(const ScreenInfoV01 &info)
 double DeltaBestPlugin::CalculateDeltaBest()
 {
 	/* Shouldn't really happen */
-	if (!best_lap.final) {
-		fprintf(out_file, "[DELTA] ! best_lap.final. Delta Best is 0.0\n");
-		return 0.0;
-	}
+	if (!best_lap.final)
+		return 0;
 
-	/*
-	   Extrapolate the best lap recording at the exact time
-	   we need to compare it with out current lap
-	*/
+	unsigned int m = last_pos;            /* Current position in meters around the track */
 
-	/* First let's calculate the best lap slope
-	   (none other than average speed in the tick) */
-	double bestSpeed, lastSpeed;
-	unsigned int t = scoring_ticks;
+	/* By using meters, and backfilling all the missing information,
+	   it shouldn't be possible to not have the exact same position in the best lap */
+	double last_time_at_pos = last_lap.elapsed[m];
+	double best_time_at_pos = best_lap.elapsed[m];
+	double delta_best = last_time_at_pos - best_time_at_pos;
 
-	if (t > 0) {
-		bestSpeed = (best_lap.distance[t] - best_lap.distance[t - 1]) /
-			(best_lap.elapsed[t] - best_lap.elapsed[t - 1]);
-		lastSpeed = (last_lap.distance[t] - last_lap.distance[t - 1]) /
-			(last_lap.elapsed[t] - last_lap.elapsed[t - 1]);
-	}
-	else {
-		bestSpeed = best_lap.distance[t] / best_lap.elapsed[t];
-		lastSpeed = last_lap.distance[t] / last_lap.elapsed[t];
-	}
+	if (delta_best > 99.0)
+		delta_best = 99.0;
+	else if (delta_best < -99)
+		delta_best = -99.0;
 
-	/* We can now use the speed to extract the distance at the exact
-	   same moment (elapsed) for the current lap */
-	double bestLapDistanceAtTime, deltaBest;
+	return delta_best;
 
-	if (t > 0) {
-		bestLapDistanceAtTime = best_lap.distance[t - 1]
-			+ (bestSpeed * (last_lap.elapsed[t] - best_lap.elapsed[t - 1]));
-	}
-	else {
-		bestLapDistanceAtTime = bestSpeed * last_lap.elapsed[t];
-	}
+	///*
+	//Extrapolate the best lap recording at the exact time
+	//we need to compare it with out current lap
+	//*/
+	//
+	///* First let's calculate the best lap slope
+	//(none other than average speed in the tick) */
+	//double bestSpeed, lastSpeed;
+	//unsigned int t = scoring_ticks;
+	//if (t > 0) {
+	//	bestSpeed = (best_lap.distance[t] - best_lap.distance[t - 1]) /
+	//		(best_lap.elapsed[t] - best_lap.elapsed[t - 1]);
+	//	lastSpeed = (last_lap.distance[t] - last_lap.distance[t - 1]) /
+	//		(last_lap.elapsed[t] - last_lap.elapsed[t - 1]);
+	//}
+	//else {
+	//	bestSpeed = best_lap.distance[t] / best_lap.elapsed[t];
+	//	lastSpeed = last_lap.distance[t] / last_lap.elapsed[t];
+	//}
 
-	/* AAh, this is a steaming pile of poo */
-	//if (lastSpeed < 0.001)
-	//	lastSpeed = 0.01;
+	///* We can now use the speed to extract the distance at the exact
+	//   same moment (elapsed) for the current lap */
+	//double bestLapDistanceAtTime, deltaBest;
 
-	deltaBest = (bestLapDistanceAtTime - last_lap.distance[t]) / bestSpeed;
+	//if (t > 0) {
+	//	bestLapDistanceAtTime = best_lap.distance[t - 1]
+	//		+ (bestSpeed * (last_lap.elapsed[t] - best_lap.elapsed[t - 1]));
+	//}
+	//else {
+	//	bestLapDistanceAtTime = bestSpeed * last_lap.elapsed[t];
+	//}
 
-	fprintf(out_file, "[DELTA] t=%d best.distance[t] = %.3f @ %.3f last.distance[t] = %.3f @ %.3f best.speed = %.3f best.atTimeX = %.3f delta = %.3f\n",
-		t, best_lap.distance[t], best_lap.elapsed[t], last_lap.distance[t], last_lap.elapsed[t],
-		bestSpeed, bestLapDistanceAtTime, deltaBest);
+	///* AAh, this is a steaming pile of poo */
+	////if (lastSpeed < 0.001)
+	////	lastSpeed = 0.01;
 
-	return deltaBest;
+	//deltaBest = (bestLapDistanceAtTime - last_lap.distance[t]) / bestSpeed;
+
+	///* Avoid ridiculous values */
+	//if (deltaBest > 99)
+	//	deltaBest = 99.0;
+	//else if (deltaBest < -99)
+	//	deltaBest = -99.0;
+
+	//fprintf(out_file, "[DELTA] t=%d best.distance[t] = %.3f @ %.3f last.distance[t] = %.3f @ %.3f best.speed = %.3f best.atTimeX = %.3f delta = %.3f\n",
+	//	t, best_lap.distance[t], best_lap.elapsed[t], last_lap.distance[t], last_lap.elapsed[t],
+	//	bestSpeed, bestLapDistanceAtTime, deltaBest);
+
+	//return deltaBest;
 }
 
 void DeltaBestPlugin::RenderScreenAfterOverlays(const ScreenInfoV01 &info)
@@ -307,15 +368,13 @@ void DeltaBestPlugin::RenderScreenAfterOverlays(const ScreenInfoV01 &info)
 	if (! NeedToDisplay())
 		return;
 
-	if (g_Font != NULL) {
-		if (deltaBest > 999)
-			deltaBest = 999.0;
-		else if (deltaBest < -999)
-			deltaBest = -999.0;
-		textColor = (deltaBest > 0) ? 0xffff0000 : 0xff00ff00;
-		sprintf(lp_deltaBest, "%+4.3f", deltaBest);
-		g_Font->DrawText(NULL, (LPCSTR)lp_deltaBest, -1, &FontPosition, DT_CENTER, textColor);
-	}
+	/* Can't draw without a font object */
+	if (g_Font == NULL)
+		return;
+
+	textColor = (deltaBest > 0) ? 0xffff0000 : 0xff00ff00;
+	sprintf(lp_deltaBest, "%+2.2f", deltaBest);
+	g_Font->DrawText(NULL, (LPCSTR)lp_deltaBest, -1, &FontPosition, DT_CENTER, textColor);
 
 }
 
