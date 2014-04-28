a8f53511c21074fb0306bf51212819d8651f578a
Cosimo Streppone <cosimo@streppone.it>
2014-04-28 22:56:42 +0200

    Reformatted source code. Ignore.

--- Include/DeltaBest.hpp
+++ Include/DeltaBest.hpp
@@ -20,7 +20,7 @@
 #define COLOR_INTENSITY         0xF0
 
 /* Toggle plugin with CTRL + a magic key. Reference:
-   http://msdn.microsoft.com/en-us/library/windows/desktop/dd375731%28v=vs.85%29.aspx */
+http://msdn.microsoft.com/en-us/library/windows/desktop/dd375731%28v=vs.85%29.aspx */
 #define MAGIC_KEY               (0x44)      /* "D" */
 #define KEY_DOWN(k)             ((GetAsyncKeyState(k) & 0x8000) && (GetAsyncKeyState(VK_CONTROL) & 0x8000))
 
@@ -30,80 +30,80 @@ class DeltaBestPlugin : public InternalsPluginV06
 
 public:
 
-	// Constructor/destructor
-	DeltaBestPlugin() {}
-	~DeltaBestPlugin() {}
+    // Constructor/destructor
+    DeltaBestPlugin() {}
+    ~DeltaBestPlugin() {}
 
-	// These are the functions derived from base class InternalsPlugin
-	// that can be implemented.
-	void Startup(long version);    // game startup
-	void Shutdown();               // game shutdown
+    // These are the functions derived from base class InternalsPlugin
+    // that can be implemented.
+    void Startup(long version);    // game startup
+    void Shutdown();               // game shutdown
 
-	void EnterRealtime();          // entering realtime
-	void ExitRealtime();           // exiting realtime
+    void EnterRealtime();          // entering realtime
+    void ExitRealtime();           // exiting realtime
 
-	void StartSession();           // session has started
-	void EndSession();             // session has ended
+    void StartSession();           // session has started
+    void EndSession();             // session has ended
 
-	// GAME OUTPUT
-	long WantsTelemetryUpdates() { return 0; }
-	void UpdateTelemetry(const TelemInfoV01 &info) { }
+    // GAME OUTPUT
+    long WantsTelemetryUpdates() { return 0; }
+    void UpdateTelemetry(const TelemInfoV01 &info) { }
 
     bool WantsGraphicsUpdates() { return false; }
-	void UpdateGraphics(const GraphicsInfoV02 &info) { }
+    void UpdateGraphics(const GraphicsInfoV02 &info) { }
 
     bool WantsToDisplayMessage(MessageInfoV01 &msgInfo);
 
-	// GAME INPUT
-	bool HasHardwareInputs() { return false; }
-	void UpdateHardware(const float fDT) { mET += fDT; }   // update the hardware with the time between frames
-	void EnableHardware() { mEnabled = true; }             // message from game to enable hardware
-	void DisableHardware() { mEnabled = false; }           // message from game to disable hardware
+    // GAME INPUT
+    bool HasHardwareInputs() { return false; }
+    void UpdateHardware(const float fDT) { mET += fDT; }   // update the hardware with the time between frames
+    void EnableHardware() { mEnabled = true; }             // message from game to enable hardware
+    void DisableHardware() { mEnabled = false; }           // message from game to disable hardware
 
     // See if the plugin wants to take over a hardware control.  If the plugin takes over the
-	// control, this method returns true and sets the value of the float pointed to by the
-	// second arg.  Otherwise, it returns false and leaves the float unmodified.
-	bool CheckHWControl(const char * const controlName, float &fRetVal) { return false;	}
-	bool ForceFeedback(float &forceValue) { return false; }
+    // control, this method returns true and sets the value of the float pointed to by the
+    // second arg.  Otherwise, it returns false and leaves the float unmodified.
+    bool CheckHWControl(const char * const controlName, float &fRetVal) { return false;	}
+    bool ForceFeedback(float &forceValue) { return false; }
 
-	// SCORING OUTPUT
-	bool WantsScoringUpdates() { return true; }
-	void UpdateScoring(const ScoringInfoV01 &info);
+    // SCORING OUTPUT
+    bool WantsScoringUpdates() { return true; }
+    void UpdateScoring(const ScoringInfoV01 &info);
 
     // COMMENTARY INPUT
-	bool RequestCommentary(CommentaryRequestInfoV01 &info) { return false; }
+    bool RequestCommentary(CommentaryRequestInfoV01 &info) { return false; }
 
-	// VIDEO EXPORT (sorry, no example code at this time)
-	virtual bool WantsVideoOutput() { return false; }
-	virtual bool VideoOpen(const char * const szFilename, float fQuality, unsigned short usFPS, unsigned long fBPS,
-		unsigned short usWidth, unsigned short usHeight, char *cpCodec = NULL) {
-		return(false);
-	} // open video output file
-	virtual void VideoClose() {}                                 // close video output file
-	virtual void VideoWriteAudio(const short *pAudio, unsigned int uNumFrames) {} // write some audio info
-	virtual void VideoWriteImage(const unsigned char *pImage) {} // write video image
+    // VIDEO EXPORT (sorry, no example code at this time)
+    virtual bool WantsVideoOutput() { return false; }
+    virtual bool VideoOpen(const char * const szFilename, float fQuality, unsigned short usFPS, unsigned long fBPS,
+        unsigned short usWidth, unsigned short usHeight, char *cpCodec = NULL) {
+            return(false);
+    } // open video output file
+    virtual void VideoClose() {}                                 // close video output file
+    virtual void VideoWriteAudio(const short *pAudio, unsigned int uNumFrames) {} // write some audio info
+    virtual void VideoWriteImage(const unsigned char *pImage) {} // write video image
 
-	// SCREEN NOTIFICATIONS
+    // SCREEN NOTIFICATIONS
 
-	void InitScreen(const ScreenInfoV01 &info);                  // Now happens right after graphics device initialization
-	void UninitScreen(const ScreenInfoV01 &info);                // Now happens right before graphics device uninitialization
+    void InitScreen(const ScreenInfoV01 &info);                  // Now happens right after graphics device initialization
+    void UninitScreen(const ScreenInfoV01 &info);                // Now happens right before graphics device uninitialization
 
-	void DeactivateScreen(const ScreenInfoV01 &info);            // Window deactivation
-	void ReactivateScreen(const ScreenInfoV01 &info);            // Window reactivation
+    void DeactivateScreen(const ScreenInfoV01 &info);            // Window deactivation
+    void ReactivateScreen(const ScreenInfoV01 &info);            // Window reactivation
 
     void RenderScreenBeforeOverlays(const ScreenInfoV01 &info){} // before rFactor overlays
-	void RenderScreenAfterOverlays(const ScreenInfoV01 &info);   // after rFactor overlays
+    void RenderScreenAfterOverlays(const ScreenInfoV01 &info);   // after rFactor overlays
 
-	void PreReset(const ScreenInfoV01 &info);					 // after detecting device lost but before resetting
-	void PostReset(const ScreenInfoV01 &info);					 // after resetting
+    void PreReset(const ScreenInfoV01 &info);					 // after detecting device lost but before resetting
+    void PostReset(const ScreenInfoV01 &info);					 // after resetting
 
     void ThreadStarted(long type) {}                             // called just after a primary thread is started (type is 0=multimedia or 1=simulation)
     void ThreadStopping(long type) {}                            // called just before a primary thread is stopped (type is 0=multimedia or 1=simulation)
 
 private:
 
-	double CalculateDeltaBest();
-	bool NeedToDisplay();
+    double CalculateDeltaBest();
+    bool NeedToDisplay();
     void WriteLog(const char * const msg);
     D3DCOLOR TextColor(double delta);
     D3DCOLOR TextColorDifferential(unsigned int t1, unsigned int t2);
@@ -115,7 +115,7 @@ private:
     //
 
     float mET;                             /* needed for the hardware example */
-	bool mEnabled;                         /* needed for the hardware example */
+    bool mEnabled;                         /* needed for the hardware example */
 
 };
 
--- Source/DeltaBest.cpp
+++ Source/DeltaBest.cpp
@@ -1,9 +1,9 @@
 /*
-  rF2 Delta Best Plugin
+rF2 Delta Best Plugin
 
-  Author: Cosimo Streppone <cosimo@streppone.it>
-  Date:   April 2014
-  URL:    http://isiforums.net/f/showthread.php/19517-Delta-Best-plugin-for-rFactor-2
+Author: Cosimo Streppone <cosimo@streppone.it>
+Date:   April 2014
+URL:    http://isiforums.net/f/showthread.php/19517-Delta-Best-plugin-for-rFactor-2
 
 */
 
@@ -16,16 +16,16 @@ extern "C" __declspec(dllexport)
     const char * __cdecl GetPluginName()               { return PLUGIN_NAME; }
 
 extern "C" __declspec(dllexport)
-PluginObjectType __cdecl GetPluginType()               { return PO_INTERNALS; }
+    PluginObjectType __cdecl GetPluginType()               { return PO_INTERNALS; }
 
 extern "C" __declspec(dllexport)
-int __cdecl GetPluginVersion()                         { return 6; }
+    int __cdecl GetPluginVersion()                         { return 6; }
 
 extern "C" __declspec(dllexport)
-PluginObject * __cdecl CreatePluginObject()            { return((PluginObject *) new DeltaBestPlugin); }
+    PluginObject * __cdecl CreatePluginObject()            { return((PluginObject *) new DeltaBestPlugin); }
 
 extern "C" __declspec(dllexport)
-void __cdecl DestroyPluginObject(PluginObject *obj)    { delete((DeltaBestPlugin *)obj); }
+    void __cdecl DestroyPluginObject(PluginObject *obj)    { delete((DeltaBestPlugin *)obj); }
 
 bool in_realtime = false;              /* Are we in cockpit? As opposed to monitor */
 bool green_flag = false;               /* Is the race in green flag condition? */
@@ -63,18 +63,18 @@ RECT FontPosition, ShadowPosition;
 void DeltaBestPlugin::WriteLog(const char * const msg)
 {
 #ifdef ENABLE_LOG
-	if (out_file == NULL)
-		out_file = fopen(LOG_FILE, "w");
+    if (out_file == NULL)
+        out_file = fopen(LOG_FILE, "w");
 
-	if (out_file != NULL)
-		fprintf(out_file, "%s\n", msg);
+    if (out_file != NULL)
+        fprintf(out_file, "%s\n", msg);
 #endif /* ENABLE_LOG */
 }
 
 void DeltaBestPlugin::Startup(long version)
 {
-	// default HW control enabled to true
-	mEnabled = true;
+    // default HW control enabled to true
+    mEnabled = true;
 }
 
 void DeltaBestPlugin::Shutdown()
@@ -100,9 +100,9 @@ void DeltaBestPlugin::EndSession()
 
 void DeltaBestPlugin::EnterRealtime()
 {
-	// start up timer every time we enter realtime
-	mET = 0.0f;
-	in_realtime = true;
+    // start up timer every time we enter realtime
+    mET = 0.0f;
+    in_realtime = true;
 #ifdef ENABLE_LOG
     WriteLog("---ENTERREALTIME---");
 #endif /* ENABLE_LOG */
@@ -110,7 +110,7 @@ void DeltaBestPlugin::EnterRealtime()
 
 void DeltaBestPlugin::ExitRealtime()
 {
-	in_realtime = false;
+    in_realtime = false;
 
     /* Reset last lap, so we'll start from scratch next time */
     last_lap.ended = NULL;
@@ -120,11 +120,11 @@ void DeltaBestPlugin::ExitRealtime()
     last_lap.elapsed[0] = 0;
 
     /* Reset delta best state */
-   	last_pos = 0;
+    last_pos = 0;
     prev_lap_dist = 0;
     //prev_current_et = 0;
     current_delta_best = 0;
- 
+
 #ifdef ENABLE_LOG
     WriteLog("---EXITREALTIME---");
 #endif /* ENABLE_LOG */
@@ -132,46 +132,46 @@ void DeltaBestPlugin::ExitRealtime()
 
 bool DeltaBestPlugin::NeedToDisplay()
 {
-	// If we're in the monitor or replay,
-	// no delta best should be displayed
-	if (! in_realtime)
-		return false;
+    // If we're in the monitor or replay,
+    // no delta best should be displayed
+    if (! in_realtime)
+        return false;
 
     // Option might be disabled by the user (TAB)
     if (! key_switch)
         return false;
 
-	// If we are in any race/practice phase that's not
-	// green flag, we don't need or want Delta Best displayed
-	if (! green_flag)
-		return false;
+    // If we are in any race/practice phase that's not
+    // green flag, we don't need or want Delta Best displayed
+    if (! green_flag)
+        return false;
 
-	/* We can't display a delta best until we have a best lap recorded */
-	if (! best_lap.final)
-		return false;
+    /* We can't display a delta best until we have a best lap recorded */
+    if (! best_lap.final)
+        return false;
 
-	return true;
+    return true;
 }
 
 void DeltaBestPlugin::UpdateScoring(const ScoringInfoV01 &info)
 {
 
-	/* No scoring updates should take place if we're
-       in the monitor as opposed to the cockpit mode */
-	if (! in_realtime)
-		return;
+    /* No scoring updates should take place if we're
+    in the monitor as opposed to the cockpit mode */
+    if (! in_realtime)
+        return;
 
     /* Toggle shortcut key. Turns off/on the display of delta time */
     if (KEY_DOWN(MAGIC_KEY))
         key_switch = ! key_switch;
 
-	/* Update plugin context information, used by NeedToDisplay() */
-	green_flag = info.mGamePhase == GREEN_FLAG;
+    /* Update plugin context information, used by NeedToDisplay() */
+    green_flag = info.mGamePhase == GREEN_FLAG;
 
-	for (long i = 0; i < info.mNumVehicles; ++i) {
-    	VehicleScoringInfoV01 &vinfo = info.mVehicle[i];
-		if (! vinfo.mIsPlayer)
-			continue;
+    for (long i = 0; i < info.mNumVehicles; ++i) {
+        VehicleScoringInfoV01 &vinfo = info.mVehicle[i];
+        if (! vinfo.mIsPlayer)
+            continue;
 
 #ifdef ENABLE_LOG
         fprintf(out_file, "mLapStartET=%.3f mCurrentET=%.3f Elapsed=%.3f mLapDist=%.3f/%.3f prevLapDist=%.3f prevCurrentET=%.3f deltaBest=%+2.2f lastPos=%d prevPos=%d\n",
@@ -187,71 +187,71 @@ void DeltaBestPlugin::UpdateScoring(const ScoringInfoV01 &info)
             prev_pos);
 #endif /* ENABLE_LOG */
 
-		/* Check if we started a new lap just now */
-		bool new_lap = (vinfo.mLapStartET != last_lap.started);
-		double curr_lap_dist = vinfo.mLapDist >= 0 ? vinfo.mLapDist : 0;
+        /* Check if we started a new lap just now */
+        bool new_lap = (vinfo.mLapStartET != last_lap.started);
+        double curr_lap_dist = vinfo.mLapDist >= 0 ? vinfo.mLapDist : 0;
 
         if (new_lap) {
-			
-			/* mLastLapTime is -1 when lap wasn't timed */
-			bool was_timed = vinfo.mLastLapTime > 0.0;
 
-			if (was_timed) {
-				last_lap.final = vinfo.mLastLapTime;
-				last_lap.ended = info.mCurrentET;
+            /* mLastLapTime is -1 when lap wasn't timed */
+            bool was_timed = vinfo.mLastLapTime > 0.0;
+
+            if (was_timed) {
+                last_lap.final = vinfo.mLastLapTime;
+                last_lap.ended = info.mCurrentET;
 
 #ifdef ENABLE_LOG
                 fprintf(out_file, "New LAP: Last = %.3f, started = %.3f, ended = %.3f, interval_offset = %.3f\n",
                     last_lap.final, last_lap.started, last_lap.ended, last_lap.interval_offset);
 #endif /* ENABLE_LOG */
 
-				/* Was it the best lap so far? */
-				bool best_so_far = (best_lap.final == NULL || (last_lap.final < best_lap.final));
-				if (best_so_far) {
+                /* Was it the best lap so far? */
+                bool best_so_far = (best_lap.final == NULL || (last_lap.final < best_lap.final));
+                if (best_so_far) {
 #ifdef ENABLE_LOG
                     fprintf(out_file, "Last lap was the best so far (final time = %.3f, previous best = %.3f)\n",
                         last_lap.final, best_lap.final);
 #endif /* ENABLE_LOG */
 
                     /* Complete the mileage of the last lap.
-                       This avoids nasty jumps into empty space (+50.xx) when later comparing with best lap */
+                    This avoids nasty jumps into empty space (+50.xx) when later comparing with best lap */
                     for (unsigned int i = last_pos + 1 ; i <= (unsigned int) info.mLapDist; i++) {
                         /* FIXME: Inaccurate. Should extrapolate last interval */
                         last_lap.elapsed[i] = last_lap.elapsed[i - 1];
                     }
 
-					best_lap = last_lap;
-				}
+                    best_lap = last_lap;
+                }
 
 #ifdef ENABLE_LOG
                 fprintf(out_file, "Best LAP yet  = %.3f, started = %.3f, ended = %.3f, interval_offset = %3.f\n",
                     best_lap.final, best_lap.started, best_lap.ended, best_lap.interval_offset);
 #endif /* ENABLE_LOG */
-			}
+            }
 
-			/* Prepare to archive the new lap */
-			last_lap.started = vinfo.mLapStartET;
-			last_lap.final = NULL;
-			last_lap.ended = NULL;
-			last_lap.interval_offset = info.mCurrentET - vinfo.mLapStartET;
-			last_lap.elapsed[0] = 0;
-			last_pos = prev_pos = 0;
-			prev_lap_dist = 0;
-			/* Leave prev_current_et alone, or you have hyper-jumps */
-		}
+            /* Prepare to archive the new lap */
+            last_lap.started = vinfo.mLapStartET;
+            last_lap.final = NULL;
+            last_lap.ended = NULL;
+            last_lap.interval_offset = info.mCurrentET - vinfo.mLapStartET;
+            last_lap.elapsed[0] = 0;
+            last_pos = prev_pos = 0;
+            prev_lap_dist = 0;
+            /* Leave prev_current_et alone, or you have hyper-jumps */
+        }
 
-		/* If there's a lap in progress, save the delta updates */
-		if (last_lap.started > 0.0) {
-			unsigned int meters = (int) (vinfo.mLapDist >= 0 ? vinfo.mLapDist : 0);
+        /* If there's a lap in progress, save the delta updates */
+        if (last_lap.started > 0.0) {
+            unsigned int meters = (int) (vinfo.mLapDist >= 0 ? vinfo.mLapDist : 0);
 
-			/* It could be that we have stopped our vehicle.
-			   In that case (same array position), we want to
-			   overwrite the previous value anyway */
-			if (meters >= last_pos) {
-				double distance_traveled = (vinfo.mLapDist - prev_lap_dist);
-				if (distance_traveled < 0)
-					distance_traveled = 0;
-				double time_interval = (info.mCurrentET - prev_current_et);
+            /* It could be that we have stopped our vehicle.
+            In that case (same array position), we want to
+            overwrite the previous value anyway */
+            if (meters >= last_pos) {
+                double distance_traveled = (vinfo.mLapDist - prev_lap_dist);
+                if (distance_traveled < 0)
+                    distance_traveled = 0;
+                double time_interval = (info.mCurrentET - prev_current_et);
 
                 if (meters == last_pos) {
                     last_lap.elapsed[meters] = info.mCurrentET - vinfo.mLapStartET;
@@ -271,53 +271,53 @@ void DeltaBestPlugin::UpdateScoring(const ScoringInfoV01 &info)
                 }
 
 #ifdef ENABLE_LOG
-				fprintf(out_file, "[DELTA] distance_traveled=%.3f time_interval=%.3f [%d .. %d]\n",
-					distance_traveled, time_interval, last_pos, meters);
+                fprintf(out_file, "[DELTA] distance_traveled=%.3f time_interval=%.3f [%d .. %d]\n",
+                    distance_traveled, time_interval, last_pos, meters);
 #endif /* ENABLE_LOG */
-			}
+            }
 
             prev_pos = last_pos;
-			last_pos = meters;
-		}
+            last_pos = meters;
+        }
 
         if (curr_lap_dist > prev_lap_dist)
             prev_lap_dist = curr_lap_dist;
 
         prev_current_et = info.mCurrentET;
-		current_delta_best = CalculateDeltaBest();
+        current_delta_best = CalculateDeltaBest();
 
-	}
+    }
 
 }
 
 void DeltaBestPlugin::InitScreen(const ScreenInfoV01& info)
 {
     /* Now we know screen X/Y, we can place the text somewhere
-       specific (in height) */
-	FontPosition.top = info.mHeight / 6.0;
-	FontPosition.left = 0;
-	FontPosition.right = info.mWidth;
-	FontPosition.bottom = info.mHeight;
+    specific (in height) */
+    FontPosition.top = info.mHeight / 6.0;
+    FontPosition.left = 0;
+    FontPosition.right = info.mWidth;
+    FontPosition.bottom = info.mHeight;
 
     ShadowPosition = FontPosition;
     ShadowPosition.top += 2;
     ShadowPosition.left += 2;
 
-	D3DXCreateFontIndirect((LPDIRECT3DDEVICE9) info.mDevice, &FontDesc, &g_Font);
-	assert(g_Font != NULL);
+    D3DXCreateFontIndirect((LPDIRECT3DDEVICE9) info.mDevice, &FontDesc, &g_Font);
+    assert(g_Font != NULL);
 
 #ifdef ENABLE_LOG
-   	WriteLog("---INIT SCREEN---");
+    WriteLog("---INIT SCREEN---");
 #endif /* ENABLE_LOG */
 
 }
 
 void DeltaBestPlugin::UninitScreen(const ScreenInfoV01& info)
 {
-	if (g_Font) {
-		g_Font->Release();
-		g_Font = NULL;
-	}
+    if (g_Font) {
+        g_Font->Release();
+        g_Font = NULL;
+    }
 #ifdef ENABLE_LOG
     WriteLog("---UNINIT SCREEN---");
 #endif /* ENABLE_LOG */
@@ -326,43 +326,43 @@ void DeltaBestPlugin::UninitScreen(const ScreenInfoV01& info)
 void DeltaBestPlugin::DeactivateScreen(const ScreenInfoV01& info)
 {
 #ifdef ENABLE_LOG
-	WriteLog("---DEACTIVATE SCREEN---");
+    WriteLog("---DEACTIVATE SCREEN---");
 #endif /* ENABLE_LOG */
 }
 
 void DeltaBestPlugin::ReactivateScreen(const ScreenInfoV01& info)
 {
 #ifdef ENABLE_LOG
-	WriteLog("---REACTIVATE SCREEN---");
+    WriteLog("---REACTIVATE SCREEN---");
 #endif /* ENABLE_LOG */
 }
 
 double DeltaBestPlugin::CalculateDeltaBest()
 {
-	/* Shouldn't really happen */
-	if (!best_lap.final)
-		return 0;
+    /* Shouldn't really happen */
+    if (!best_lap.final)
+        return 0;
 
-	unsigned int m = last_pos;            /* Current position in meters around the track */
+    unsigned int m = last_pos;            /* Current position in meters around the track */
 
-	/* By using meters, and backfilling all the missing information,
-	   it shouldn't be possible to not have the exact same position in the best lap */
-	double last_time_at_pos = last_lap.elapsed[m];
-	double best_time_at_pos = best_lap.elapsed[m];
-	double delta_best = last_time_at_pos - best_time_at_pos;
+    /* By using meters, and backfilling all the missing information,
+    it shouldn't be possible to not have the exact same position in the best lap */
+    double last_time_at_pos = last_lap.elapsed[m];
+    double best_time_at_pos = best_lap.elapsed[m];
+    double delta_best = last_time_at_pos - best_time_at_pos;
 
-	if (delta_best > 99.0)
-		delta_best = 99.0;
-	else if (delta_best < -99)
-		delta_best = -99.0;
+    if (delta_best > 99.0)
+        delta_best = 99.0;
+    else if (delta_best < -99)
+        delta_best = -99.0;
 
-	return delta_best;
+    return delta_best;
 }
 
 bool DeltaBestPlugin::WantsToDisplayMessage( MessageInfoV01 &msgInfo )
 {
     /* Wait until we're in realtime, otherwise
-       the message is lost in space */
+    the message is lost in space */
     if (! in_realtime)
         return false;
 
@@ -383,24 +383,24 @@ bool DeltaBestPlugin::WantsToDisplayMessage( MessageInfoV01 &msgInfo )
 
 void DeltaBestPlugin::RenderScreenAfterOverlays(const ScreenInfoV01 &info)
 {
-	char lp_deltaBest[15] = "";
-	double deltaBest = current_delta_best;
+    char lp_deltaBest[15] = "";
+    double deltaBest = current_delta_best;
 
-	/* If we're not in realtime, not in green flag, etc...
-	   there's no need to display the Delta Best time */
-	if (! NeedToDisplay())
-		return;
+    /* If we're not in realtime, not in green flag, etc...
+    there's no need to display the Delta Best time */
+    if (! NeedToDisplay())
+        return;
 
-	/* Can't draw without a font object */
-	if (g_Font == NULL)
-		return;
+    /* Can't draw without a font object */
+    if (g_Font == NULL)
+        return;
 
     //
     // Try to draw a quad on to the screen, but where exactly?
     // This should draw a background grey rectangle behind the delta time.
     //
 #if 0
-    #define CUSTOMFVF (D3DFVF_XYZRHW | D3DFVF_DIFFUSE)
+#define CUSTOMFVF (D3DFVF_XYZRHW | D3DFVF_DIFFUSE)
     struct CUSTOMVERTEX {
         FLOAT X, Y, Z;
         D3DCOLOR COLOR;
@@ -434,9 +434,9 @@ void DeltaBestPlugin::RenderScreenAfterOverlays(const ScreenInfoV01 &info)
     D3DCOLOR textColor = TextColor(deltaBest);
     //D3DCOLOR textColor = TextColorDifferential3();
 
-	sprintf(lp_deltaBest, "%+2.2f", deltaBest);
+    sprintf(lp_deltaBest, "%+2.2f", deltaBest);
     g_Font->DrawText(NULL, (LPCSTR)lp_deltaBest, -1, &ShadowPosition, DT_CENTER, shadowColor);
-	g_Font->DrawText(NULL, (LPCSTR)lp_deltaBest, -1, &FontPosition,   DT_CENTER, textColor);
+    g_Font->DrawText(NULL, (LPCSTR)lp_deltaBest, -1, &FontPosition,   DT_CENTER, textColor);
 }
 
 /* Simple style: negative delta = green, positive delta = red */
@@ -469,8 +469,8 @@ D3DCOLOR DeltaBestPlugin::TextColorDifferential3()
     static const D3DCOLOR ALPHA = 0xD0000000;
 
     /* Calculate the derivative of the best lap and the last lap.
-       If the last lap derivative grows faster, then we're losing ground.
-       If the last lap derivative grows slower, we're gaining compared to best lap. */
+    If the last lap derivative grows faster, then we're losing ground.
+    If the last lap derivative grows slower, we're gaining compared to best lap. */
 
     unsigned int t2 = last_pos;
     unsigned int t1 = prev_pos;
@@ -509,12 +509,12 @@ D3DCOLOR DeltaBestPlugin::TextColorDifferential3()
 
 void DeltaBestPlugin::PreReset(const ScreenInfoV01 &info)
 {
-	if (g_Font)
-		g_Font->OnLostDevice();
+    if (g_Font)
+        g_Font->OnLostDevice();
 }
 
 void DeltaBestPlugin::PostReset(const ScreenInfoV01 &info)
 {
-	if (g_Font)
-		g_Font->OnResetDevice();
+    if (g_Font)
+        g_Font->OnResetDevice();
 }
